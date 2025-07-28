2720b43fd38b08e3b7ff9242ce985e64134911be
Vishmay Dhruv <tatvasoft.pci238@outlook.com>
2025-07-28 09:18:35 +0530

    updated dynamic-form file

--- src/app/shared/components/dynamic-form/dynamic-form.ts
+++ src/app/shared/components/dynamic-form/dynamic-form.ts
@@ -3,66 +3,51 @@ import { FieldConfigInterface } from '../../../core/models/field-config.interfac
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CustomInput } from '../custom-input/custom-input';
 import { CustomButton } from '../custom-button/custom-button';
-import { CustomButtonInterface } from '../../../core/models/custom-button.interface';
-import { PasswordStrengthValidator } from '../../validators/password-strength.validator';
-import { MatchFieldsValidator } from '../../validators/match-fields.validator';
-import { MatError } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-dynamic-form',
-  imports: [ReactiveFormsModule, CustomInput, CustomButton, MatError],
+  imports: [ReactiveFormsModule, CustomInput, CustomButton],
   templateUrl: './dynamic-form.html',
   styleUrl: './dynamic-form.scss'
 })
-export class DynamicForm implements OnInit {
+export class DynamicForm implements OnInit{
   @Input() formJson: FieldConfigInterface[] = [];
-  @Input() buttonConfig: CustomButtonInterface = {
-    type: 'submit',
-    label: 'Submit',
-    style: 'success',
-    size: 'lg',
-    icon: 'send',
-    block: true,
-    class: 'mt-3',
-  };
   @Output() formSubmit: EventEmitter<any> = new EventEmitter<any>();
 
-  form!: FormGroup;
-  fb = inject(FormBuilder);
+  form!:FormGroup;
+  fb=inject(FormBuilder);
 
   ngOnInit(): void {
     this.form = this.buildForm(this.formJson);
   }
 
-  buildForm(config: FieldConfigInterface[]): FormGroup {
-    const group: { [Key: string]: any } = {};
-    const matchFields: { field: string; matchesWith: string }[] = [];
+  buildForm(config:FieldConfigInterface[]) :FormGroup {
+    const group: { [Key:string]: any } = {};
 
-    for (let field of config) {
+    for(let field of config) {
       const validators = this.mapValidators(field.validators!);
-      group[field.inputfield.name] = this.fb.control(field.inputfield.value || '', validators);
+      const name = field.inputfield.name;
+      const type = field.inputfield.type;
+      const value = field.inputfield.value;
+      let defaultValue = this.getDefaultValue(type, value);
+      group[name] = this.fb.control(defaultValue, validators);
     }
 
     return this.fb.group(group);
   }
 
-  mapValidators(validators: string[]): any[] {
+  mapValidators(validators: string[]): any[]{
     const formValidators = [];
 
-    if (validators) {
-      for (let val of validators) {
-        if (val == 'required') {
+    if(validators){
+      for(let val of validators){
+        if(val == 'required'){
           formValidators.push(Validators.required);
-        } else if (val == 'email') {
+        } else if(val == 'email'){
           formValidators.push(Validators.email);
-        } else if (val.startsWith('min:')) {
+        } else if(val.startsWith('min:')){
           const valNum = parseInt(val.split(':')[1]);
-          formValidators.push(Validators.minLength(valNum));
-        } else if (val.startsWith('max:')) {
-          const valNum = parseInt(val.split(':')[1]);
-          formValidators.push(Validators.max(valNum));
-        } else if (val == 'missingUpperCase' || val == 'missingLowerCase' || val == 'missingDigit' || val == 'missingSpecialCharacter') {
-          formValidators.push(PasswordStrengthValidator());
+          formValidators.push(Validators.min(valNum));
         }
       }
     }
@@ -80,6 +65,19 @@ export class DynamicForm implements OnInit {
 
   getFormControl(name: string): FormControl {
     return this.form.get(name) as FormControl;
+  }
+
+  getDefaultValue(type:string, value:any){
+    if(value !== undefined) return value;
+
+    switch(type) {
+      case 'checkbox':
+        return false;
+      case 'number':
+        return 0;
+      default:
+        return '';
+    }
   }
 
 }
